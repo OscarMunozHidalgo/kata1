6e91482e32f8a5dd62e07e5543dc3f9bea0a4f1a
Oscar <oscar.munoz103@alu.ulpgc.es>
2023-10-15 21:23:43 +0100

    #Worker class created

---
+++ src/pckg/kata1/Worker.java
@@ -0,0 +1,37 @@
+package pckg.kata1;
+
+public class Worker {
+    private String name;
+    private int hours;
+    private int payment;
+
+    public Worker(String name, int hours, int payment) {
+        this.name = name;
+        this.hours = hours;
+        this.payment = payment;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHours() {
+        return hours;
+    }
+
+    public int getPayment() {
+        return payment;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+
+    public void setPayment(int payment) {
+        this.payment = payment;
+    }
+}
